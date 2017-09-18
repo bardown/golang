91d872565adc890d9e6e22c86915a6600a2c5cad
Lucas Wojcik <techlucas@gmail.com>
2017-09-18 07:27:47 -0500

    random stuff...

--- misc/main.go
+++ misc/main.go
@@ -2,9 +2,22 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"os/exec"
 )
+
+type Player struct {
+	Name        string
+	GamesPlayed int
+	Goals       int
+	Assists     int
+	Points      int
+}
+
+type Game struct {
+	Vs string
+	GF int
+	GA int
+}
 
 func pipedCMD() {
 	cmd := "echo | systemctl status mariadb.service"
@@ -16,20 +29,22 @@ func pipedCMD() {
 	fmt.Println(string(output))
 }
 
-func waitForMe(stressApp string) (results string) {
-	cmd := exec.Command(stressAppp)
-
-	if err := cmd.Start(); err != nil {
-		fmt.Printf("Error not nil on cmd.Start() for sleep 5: %v\n", err)
-	}
-	log.Printf("Waiting for command to finish...")
-
-	if err := cmd.Wait(); err != nil {
-		log.Fatal(fmt.Sprintf("Command finished with error: %v\n", err))
-	}
-	fmt.Printf("Command finsihed with great success!\n")
-}
+//func waitForMe(stressApp string) (results string) {
+//	cmd := exec.Command(stressAppp)
+//
+//	if err := cmd.Start(); err != nil {
+//		fmt.Printf("Error not nil on cmd.Start() for sleep 5: %v\n", err)
+//	}
+//	log.Printf("Waiting for command to finish...")
+//
+//	if err := cmd.Wait(); err != nil {
+//		log.Fatal(fmt.Sprintf("Command finished with error: %v\n", err))
+//	}
+//	fmt.Printf("Command finsihed with great success!\n")
+//}
 
 func main() {
-	waitForMe()
+	Lucas := Player{Name: "Lucas", Goals: 5, Assists: 3, Points: 8, GamesPlayed: 2}
+
+	fmt.Printf("Name: %s, Goals: %d, Assists: %d, Points: %d, GamesPlayed: %d", Lucas.Name, Lucas.Goals, Lucas.Assists, Lucas.Points, Lucas.GamesPlayed)
 }
